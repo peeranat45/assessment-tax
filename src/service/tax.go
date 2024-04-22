3383778f4db0d8810d3ad04de8bd6304acb855a0
Peeranat <peeranat45@gmail.com>
2024-04-22 18:26:04 +0700

    tax struct

---
+++ src/service/tax.go
@@ -0,0 +1,21 @@
+package service
+
+type Allowance struct {
+	AllowanceType	string		`json:"allowanceType"`
+	Amount			float64		`json:"amount"`
+}
+
+type TaxCalculationRequest struct {
+	TotalIncome		float64		`json:"totalIncome"`
+	Wht				float64		`json:"wht"`
+	Allowances		[]Allowance	`json:"allowances"`	
+}
+
+type TaxCalculationResponse struct {
+	Tax				float64		`json:"tax"`
+}
+
+
+type TaxService interface {
+	TaxCalculation(TaxCalculationRequest) (*TaxCalculationResponse, error)
+}
