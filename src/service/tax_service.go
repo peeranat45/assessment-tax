ca669e9a7e44f68af8e950e9e2248275ffadf4d9
Peeranat <peeranat45@gmail.com>
2024-04-25 16:22:53 +0700

    cover almost all testcase

--- src/service/tax_service.go
+++ src/service/tax_service.go
@@ -1,9 +1,21 @@
 package service
 
+// import "errors"
+
 
 func TaxCalculation(request TaxCalculationRequest) (*TaxCalculationResponse, error) {
-	response := TaxCalculationResponse{
-		Tax: 29000,
+	
+	if request.TotalIncome <= 150000 {
+		return &TaxCalculationResponse{Tax: 0}, nil
+	}else if request.TotalIncome <= 500000{
+		return &TaxCalculationResponse{Tax: 0.10 * (request.TotalIncome - 150000 - 60000)}, nil
+	}else if request.TotalIncome <= 1000000{
+		return &TaxCalculationResponse{Tax: 0.15 * (request.TotalIncome - 500000 - 60000)}, nil
+	}else if request.TotalIncome <= 2000000{
+		return &TaxCalculationResponse{Tax: 0.20 * (request.TotalIncome - 1000000 - 60000)}, nil
+	}else if request.TotalIncome > 2000000{
+		return &TaxCalculationResponse{Tax: 0.35 * (request.TotalIncome - 2000000 - 60000)}, nil
 	}
-	return &response, nil
+
+	return nil, nil
 } 
--- src/service/tax_test.go
+++ src/service/tax_test.go
@@ -28,4 +28,88 @@ func TestCalculateTaxWithoutWHT(t *testing.T) {
 		assert.Equal(t, want, *got)
 
 	})
+
+	t.Run("Cal Tax less than 150000 ", func(t *testing.T) {
+		want := TaxCalculationResponse{
+			Tax: 0,
+		}
+		got, err := TaxCalculation(TaxCalculationRequest{
+			TotalIncome: 150000.0,
+			Wht: 0,
+			Allowances: []Allowance{
+				{
+					AllowanceType: "donation",
+					Amount: 0,
+				},
+			},
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, got)
+
+		assert.Equal(t, want, *got)
+	})
+
+	t.Run("Cal Tax less than 1000000 ", func(t *testing.T) {
+		want := TaxCalculationResponse{
+			Tax: 66000,
+		}
+		got, err := TaxCalculation(TaxCalculationRequest{
+			TotalIncome: 1000000.0,
+			Wht: 0,
+			Allowances: []Allowance{
+				{
+					AllowanceType: "donation",
+					Amount: 0,
+				},
+			},
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, got)
+
+		assert.Equal(t, want, *got)
+	})
+
+	t.Run("Cal Tax less than 2000000 ", func(t *testing.T) {
+		want := TaxCalculationResponse{
+			Tax: 188000,
+		}
+		got, err := TaxCalculation(TaxCalculationRequest{
+			TotalIncome: 2000000.0,
+			Wht: 0,
+			Allowances: []Allowance{
+				{
+					AllowanceType: "donation",
+					Amount: 0,
+				},
+			},
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, got)
+
+		assert.Equal(t, want, *got)
+	})
+
+	// t.Run("Cal tax more than 5000000", func (t *testing.T) {
+	// 	want := TaxCalculationResponse{
+	// 		Tax: 20790000,
+	// 	}
+	// 	got, err := TaxCalculation(TaxCalculationRequest{
+	// 		TotalIncome: 8000000.0,
+	// 		Wht: 0,
+	// 		Allowances: []Allowance{
+	// 			{
+	// 				AllowanceType: "donation",
+	// 				Amount: 0,
+	// 			},
+	// 		},
+	// 	})
+
+	// 	assert.Nil(t, err)
+	// 	assert.NotNil(t, got)
+
+	// 	assert.Equal(t, want, *got)
+	// })
 }
